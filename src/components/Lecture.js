4633368c089283b666bf970ecc303453015c613a
Nicolas <nico.daddabbo7100@gmail.com>
2023-07-13 12:46:27 +0200

    ajout video twitter

--- src/components/Lecture.js
+++ src/components/Lecture.js
@@ -1,7 +1,8 @@
-import React, { Fragment, useState } from "react";
+import React, { Fragment } from "react";
 import { ImageList } from "./Pages";
 import ListeDeroulante from "./ListeDeroulante";
 import "../style/style.css";
+import { TwitterVideoEmbed } from "react-twitter-embed";
 
 const Lecture = () => {
   return (
@@ -9,10 +10,25 @@ const Lecture = () => {
       <ListeDeroulante imageList={ImageList} />
       {ImageList.map((img) => (
         <div key={img.id} className="page" id={img.id}>
-          <img src={img.image} className={img.double ? "d" : "image"} />
-          <p  className="num-page">{img.num}</p>
+          <img
+            src={img.image}
+            alt={img.id}
+            className={img.double ? "d" : "image"}
+          />
+          <p className="num-page">{img.num}</p>
         </div>
       ))}
+
+      <div className="preview">
+        <h3>Preview du prochain chapitre</h3>
+        <TwitterVideoEmbed
+          id={"1679147795687809024"}
+          options={{
+            height: "100px"
+          }}
+          className="video-preview"
+        />
+      </div>
     </Fragment>
   );
 };
