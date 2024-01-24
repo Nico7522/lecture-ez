028a1ae980e031f86a3302f7009125e56821efb0
Nicolas <nico.daddabbo7100@gmail.com>
2024-01-24 13:05:29 +0100

    ajout chapitre 272

--- src/components/Lecture.js
+++ src/components/Lecture.js
@@ -15,9 +15,9 @@ const Lecture = () => {
     let pageNumber = 1;
     let idNumber = 1;
     let temp = [];
-    while (i < 38) {
+    while (i < 57) {
    
-      if (i === 20) {
+      if (i === 20 || i === 38) {
         pageNumber = 1;
         idNumber = 1;
       }
@@ -25,7 +25,7 @@ const Lecture = () => {
         imgUrl: `${process.env.REACT_APP_IMG_URL}${imgNumber}.png`,
         num : pageNumber,
         id: idNumber,
-        double: i === 16 || i === 27 || i === 34 ? true : false
+        double: i === 16 || i === 27 || i === 34 || i === 47 ? true : false
       })
       pageNumber++;
       imgNumber++;
